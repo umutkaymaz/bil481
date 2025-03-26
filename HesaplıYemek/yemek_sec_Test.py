c9cc8e66f74b888e1e8429ca462265f893260d0b
skydfire <kemalceylantr@gmail.com>
2025-03-26 22:40:33 +0300

    bazı testler için logger yazıldı

--- HesaplıYemek/yemek_sec_Test.py
+++ HesaplıYemek/yemek_sec_Test.py
@@ -19,22 +19,28 @@ config.menu = ttk.Treeview(root, columns=("Yemek",), show="headings")
 config.menu.heading("Yemek", text="Yemek")
 config.menu.pack()
 
-pseudo_sepet = "Cheeseburger"
-pseudo_amount = 10
 
-item_id = config.menu.insert("", "end", values=(pseudo_sepet,))
-config.menu.selection_set(item_id)
+
 
 pseudo_food_var = tk.StringVar(root)
 pseudo_miktar_var = tk.IntVar(root)
 
-@pytest.mark.parametrize("sepet, selected_food_var, miktar_var, sonuc1, sonuc2", [
-    ({"Cheeseburger": 10}, pseudo_food_var, pseudo_miktar_var, "Cheeseburger", 10),
-    ({"Pizza": 5}, pseudo_food_var, pseudo_miktar_var, "Pizza", 5),
-    ({"Doner": 3}, pseudo_food_var, pseudo_miktar_var, "Doner", 3),
+@pytest.mark.parametrize("sepet, selected_food_var, miktar_var,yemek_adı, sonuc1, sonuc2", [
+    ({"Cheeseburger": 10}, pseudo_food_var, pseudo_miktar_var,"Cheeseburger","Cheeseburger", 10),
+    ({"Pizza": 5}, pseudo_food_var, pseudo_miktar_var,"Pizza", "Pizza", 5),
+    ({"Doner": 3}, pseudo_food_var, pseudo_miktar_var, "Doner","Doner", 3),
+    ({"Lahmacun": 7}, pseudo_food_var, pseudo_miktar_var, "Lahmacun", "Lahmacun", 7),
+    ({"Burger": 2}, pseudo_food_var, pseudo_miktar_var, "Burger", "Burger", 2),
+    ({"Kebap": 4}, pseudo_food_var, pseudo_miktar_var, "Kebap", "Kebap", 4),
+    ({"Sushi": 8}, pseudo_food_var, pseudo_miktar_var, "Sushi", "Sushi", 8),
+    ({"Taco": 6}, pseudo_food_var, pseudo_miktar_var, "Taco", "Taco", 6),
+    ({"Salad": 1}, pseudo_food_var, pseudo_miktar_var, "Salad", "Salad", 1)
 ])
-def test_yemek_sec(sepet: dict, selected_food_var: tk.StringVar, miktar_var: tk.IntVar, sonuc1, sonuc2):
+def test_yemek_sec(sepet: dict, selected_food_var: tk.StringVar, miktar_var: tk.IntVar, yemek_adı,sonuc1, sonuc2):
     
+    pseudo_sepet = yemek_adı
+    item_id = config.menu.insert("", "end", values=(pseudo_sepet))
+    config.menu.selection_set(item_id)
     yemek_sec(sepet, selected_food_var, miktar_var)
     obtained_food = selected_food_var.get()
     obtained_amount = miktar_var.get()
