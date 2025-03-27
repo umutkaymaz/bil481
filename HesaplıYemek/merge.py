5ba4bdb58d07fc9d3822c9c6a417e014bd62e1e7
skydfire <kemalceylantr@gmail.com>
2025-03-27 12:34:59 +0300

    web scraping entegresi

--- HesaplıYemek/merge.py
+++ HesaplıYemek/merge.py
@@ -2,6 +2,7 @@ import config
 from yemeksepeti_food_scraper import yemeksepeti_food_scrape
 from getiryemek_food_scraper import getiryemek_restaurant_scrape
 import pandas as pd
+
 def menu_al(path_csv):
     konumx = config.konum_girisi.get()
     lokantax = config.restoran_girisi.get()
